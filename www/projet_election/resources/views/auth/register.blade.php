bc4528e0c1c6ff1c771c7e658c4948e46f3f75de
Nayash03 <shayan.cahelo@edu.esiee-it.fr>
2025-06-17 10:00:00 +0200

    Add | password_toogle for register blade

--- www/projet_election/resources/views/auth/register.blade.php
+++ www/projet_election/resources/views/auth/register.blade.php
@@ -57,15 +57,24 @@
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <div class="input-group">
+                                    <input id="password" type="password"
+                                        class="form-control @error('password') is-invalid @enderror"
+                                        name="password" required autocomplete="new-password">
+
+                                    <button type="button" class="btn btn-outline-secondary" onclick="togglePassword()">
+                                        👁️
+                                    </button>
+                                </div>
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
+                                    <span class="invalid-feedback d-block" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
                         </div>
+
 
                         <div class="row mb-3">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
